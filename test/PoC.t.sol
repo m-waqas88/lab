183329ba65114f7c7d92e476afd3d0efe04e6766
Waqas Muhammad <m.waqas171288@gmail.com>
2025-07-15 13:40:35 +0500

    cleaning

---
+++ test/PoC.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+
+
+contract PoC is Test {
+
+    function setUp() public {
+
+    }
+
+    function test_test() public {
+
+    }
+
+}
